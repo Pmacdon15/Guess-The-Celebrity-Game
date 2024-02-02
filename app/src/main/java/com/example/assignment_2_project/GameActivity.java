9bf0009224279f4b9822cacc1c10d34a8abcd1de
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-02-02 15:18:24 -0700

    answers now in random order.

--- app/src/main/java/com/example/assignment_2_project/GameActivity.java
+++ app/src/main/java/com/example/assignment_2_project/GameActivity.java
@@ -8,6 +8,9 @@ import android.widget.ImageView;
 import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class GameActivity extends AppCompatActivity {
 
     private ImageView imageViewCelebrity;
@@ -16,17 +19,18 @@ public class GameActivity extends AppCompatActivity {
     private Button buttonAnswer3;
     private Button buttonAnswer4;
     private Button buttonNext;
-    private Button[] buttons;
-    private final String[] correctGuesses = {"Bill Cosby", "Mike Tyson", "Kid Rock", "Robert Downey Jr.","Charlie Sheen"};
+    //private Button[] buttons;
+    private ArrayList<Button> buttons;
+    //private final String[] correctGuesses = {"Bill Cosby", "Mike Tyson", "Kid Rock", "Robert Downey Jr.","Charlie Sheen"};
     private final int[] resourceFiles = {R.drawable.cosby, R.drawable.tyson, R.drawable.kidrock, R.drawable.rdj, R.drawable.charliesheen};
 
     // 2D array of incorrect guesses for each round
-    private final String[][] incorrectGuesses = {
-            {"Bill Nye", "Bill Gates", "Bill Clinton"},
-            {"Bill Murray", "Billie Eilish", "Billie Joe Armstrong"},
-            {"Billie Holiday", "Billy Joel", "Billy Ray Cyrus"},
-            {"Billy Idol", "Billy Bob Thornton", "Billy Crystal"},
-            {"Billy Dee Williams", "Billy Zane", "Billy Corgan"}
+    private final String[][] guesses = {
+            {"Bill Cosby", "Bill Nye", "Bill Gates", "Bill Clinton"},
+            {"Mike Tyson", "Bill Murray", "Billie Eilish", "Billie Joe Armstrong"},
+            {"Kid Rock", "Billie Holiday", "Billy Joel", "Billy Ray Cyrus"},
+            {"Robert Downey Jr.", "Billy Idol", "Billy Bob Thornton", "Billy Crystal"},
+            {"Charlie Sheen", "Billy Dee Williams", "Billy Zane", "Billy Corgan"}
     };
 
 
@@ -42,8 +46,15 @@ public class GameActivity extends AppCompatActivity {
         buttonAnswer4 = findViewById(R.id.button_Answer4);
         buttonNext = findViewById(R.id.button_Next);
 
+        // Initialize the buttons ArrayList
+        buttons = new ArrayList<>();
+        buttons.add(buttonAnswer1);
+        buttons.add(buttonAnswer2);
+        buttons.add(buttonAnswer3);
+        buttons.add(buttonAnswer4);
+
         // Initialize the buttons array after setContentView()
-        buttons = new Button[]{buttonAnswer1, buttonAnswer2, buttonAnswer3, buttonAnswer4};
+        //buttons = new Button[]{buttonAnswer1, buttonAnswer2, buttonAnswer3, buttonAnswer4};
 
         setupButtons(0);
 
@@ -57,26 +68,31 @@ public class GameActivity extends AppCompatActivity {
 
 
     private void setupButtons(int round) {
-
+        // Change the text of the next button to "Finish" if it's the last round
         if (round == 4) {
             buttonNext.setText(R.string.finish);
         }
 
+        // Set the image of the celebrity
         imageViewCelebrity.setImageResource(resourceFiles[round]);
-        buttonAnswer1.setText(correctGuesses[round]);
-        buttonAnswer2.setText(incorrectGuesses[round][0]);
-        buttonAnswer3.setText(incorrectGuesses[round][1]);
-        buttonAnswer4.setText(incorrectGuesses[round][2]);
 
-        buttonAnswer1.setBackgroundColor(getResources().getColor(R.color.blue, null));
-        buttonAnswer2.setBackgroundColor(getResources().getColor(R.color.blue, null));
-        buttonAnswer3.setBackgroundColor(getResources().getColor(R.color.blue, null));
-        buttonAnswer4.setBackgroundColor(getResources().getColor(R.color.blue, null));
+        // Shuffling the buttons
+        Collections.shuffle(buttons);
+
+        // Loop through the buttons and set the text
+        for (int i = 0; i < buttons.size(); i++) {
+            buttons.get(i).setText(guesses[round][i]);
+        }
+
+        // Set the button color blue for each button
+        for (Button button : buttons) {
+            button.setBackgroundColor(getResources().getColor(R.color.blue, null));
+        }
 
         // Set up click listeners for each button
         for (Button button : buttons) {
             button.setOnClickListener(view -> {
-                if (button.getText().toString().equals(correctGuesses[round])) {
+                if (button.getText().toString().equals(guesses[round][0])) {
                     // This is the correct answer
                     button.setBackgroundColor(getResources().getColor(R.color.green, null));
                     button.setText(R.string.correct);
