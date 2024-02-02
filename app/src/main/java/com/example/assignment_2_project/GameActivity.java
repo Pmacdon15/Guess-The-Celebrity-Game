e18418589fd6fd9b7fc8d90eb6c53715b1a3a7dd
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-02-01 20:31:39 -0700

    game now has two images

--- app/src/main/java/com/example/assignment_2_project/GameActivity.java
+++ app/src/main/java/com/example/assignment_2_project/GameActivity.java
@@ -3,17 +3,22 @@ package com.example.assignment_2_project;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GameActivity extends AppCompatActivity {
 
+    private ImageView imageViewCelebrity;
     private Button buttonAnswer1;
     private Button buttonAnswer2;
     private Button buttonAnswer3;
-    private Button buttonNextRound;
+    private Button buttonAnswer4;
+    private Button buttonNext;
     private final String[] correctGuesses = {"Bill Cosby", "Mike Tyson"};
+    private final int[] resourceFiles = {R.drawable.cosby, R.drawable.tyson};
+
     // 2D array of incorrect guesses for each round
     private final String[][] incorrectGuesses = {
             {"Bill Nye", "Bill Gates", "Bill Clinton"},
@@ -29,11 +34,12 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        imageViewCelebrity = findViewById(R.id.imageView_Celebrity);
         buttonAnswer1 = findViewById(R.id.button_Answer1);
         buttonAnswer2 = findViewById(R.id.button_Answer2);
         buttonAnswer3 = findViewById(R.id.button_Answer3);
-        buttonNextRound = findViewById(R.id.button_Answer4);
-        Button buttonNext = findViewById(R.id.button_Next);
+        buttonAnswer4 = findViewById(R.id.button_Answer4);
+        buttonNext = findViewById(R.id.button_Next);
 
         setupButtons(0);
 
@@ -48,20 +54,27 @@ public class GameActivity extends AppCompatActivity {
 
 
     private void setupButtons(int round) {
+        imageViewCelebrity.setImageResource(resourceFiles[round]);
         buttonAnswer1.setText(correctGuesses[round]);
         buttonAnswer2.setText(incorrectGuesses[round][0]);
         buttonAnswer3.setText(incorrectGuesses[round][1]);
-        buttonNextRound.setText(incorrectGuesses[round][2]);
+        buttonAnswer4.setText(incorrectGuesses[round][2]);
+
+        buttonAnswer1.setBackgroundColor(getResources().getColor(R.color.blue, null));
+        buttonAnswer2.setBackgroundColor(getResources().getColor(R.color.blue, null));
+        buttonAnswer3.setBackgroundColor(getResources().getColor(R.color.blue, null));
+        buttonAnswer4.setBackgroundColor(getResources().getColor(R.color.blue, null));
+
 
         buttonAnswer1.setOnClickListener(view -> {
             buttonAnswer1.setText(R.string.correct);
             buttonAnswer2.setText("");
             buttonAnswer3.setText("");
-            buttonNextRound.setText("");
+            buttonAnswer4.setText("");
             buttonAnswer1.setBackgroundColor(getResources().getColor(R.color.green, null));
             buttonAnswer2.setBackgroundColor(getResources().getColor(R.color.red, null));
             buttonAnswer3.setBackgroundColor(getResources().getColor(R.color.red, null));
-            buttonNextRound.setBackgroundColor(getResources().getColor(R.color.red, null));
+            buttonAnswer4.setBackgroundColor(getResources().getColor(R.color.red, null));
         });
 
     }
