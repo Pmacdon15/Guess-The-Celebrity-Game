66b03df59b2aac7a39ce37118b841e3439384271
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-02-29 17:53:21 -0700

    broke it in to smaller functions and cleaned it up

--- app/src/main/java/com/example/assignment_2_project/GameActivity.java
+++ app/src/main/java/com/example/assignment_2_project/GameActivity.java
@@ -18,13 +18,13 @@ import java.util.Collections;
 public class GameActivity extends AppCompatActivity {
     private ImageView imageViewCelebrity;
     private Button buttonNext;
-    private static ArrayList<Button> buttons;
+    private Button buttonAnswer1, buttonAnswer2, buttonAnswer3, buttonAnswer4;
+    private ArrayList<Button> buttons;
     private final int[] resourceFiles = {R.drawable.cosby, R.drawable.tyson, R.drawable.kidrock, R.drawable.rdj, R.drawable.charliesheen};
     private static int correctAnswer = 0; // Index of the correct answer
     private static int incorrectAnswer = 0; // Index of the incorrect answer
+    // To use lambda expression, the variable must be final so created an array to store the value which can be changed
     private static final int[] round = {0}; // Index of the current round
-
-    private static boolean flagSavedInstance = false;
 
     // 2D array of guesses for each round
     // First Name in each row is the correct answer
@@ -41,45 +41,26 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        // Change status bar color
-        Window window = getWindow();
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        window.setStatusBarColor(Color.parseColor("#1984BF"));
+        Log.d("GameActivity", "Setting up buttons for round " + round[0]);
 
-        imageViewCelebrity = findViewById(R.id.imageView_Celebrity);
-        Button buttonAnswer1 = findViewById(R.id.button_Answer1);
-        Button buttonAnswer2 = findViewById(R.id.button_Answer2);
-        Button buttonAnswer3 = findViewById(R.id.button_Answer3);
-        Button buttonAnswer4 = findViewById(R.id.button_Answer4);
-        buttonNext = findViewById(R.id.button_Next);
-
-        // Initialize the buttons ArrayList
-        buttons = new ArrayList<>();
-        buttons.add(buttonAnswer1);
-        buttons.add(buttonAnswer2);
-        buttons.add(buttonAnswer3);
-        buttons.add(buttonAnswer4);
+        setHeaderColor();
+        setFields();
+        createButtonArray();
+        setButtonColor();
+        setOnClicks();
+        setNextButton();
+        setNextButtonIfLastRound();
+        setImageViewCelebrity();
 
         // Restore the state if available
-        if (savedInstanceState == null) {
-            // Set up the buttons for the first round only if savedInstanceState is null
-            Log.d("GameActivity", "Setting up buttons for round " + round[0]);
-            setupButtons(round[0]);
-        }
+        if (savedInstanceState == null) setupButtonsTxtNotOnRotate();
 
-        buttonNext.setOnClickListener(view -> {
-            // Log.d("GameActivity", "Next button clicked");
-            round[0]++;
-            setupButtons(round[0]);
-        });
     }
+
     // testing
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
-
-        flagSavedInstance = true;
         // Save the text of each button
         for (int i = 0; i < buttons.size(); i++) {
             outState.putString("buttonText" + i, buttons.get(i).getText().toString());
@@ -89,7 +70,6 @@ public class GameActivity extends AppCompatActivity {
     @Override
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-
         // Restore the state only if buttons ArrayList is initialized
         if (buttons != null) {
             // Restore the text of each button
@@ -97,54 +77,56 @@ public class GameActivity extends AppCompatActivity {
                 String buttonText = savedInstanceState.getString("buttonText" + i);
                 buttons.get(i).setText(buttonText);
             }
+            setNextButtonIfLastRound();
+            setImageViewCelebrity();
+            setButtonColor();
         }
-        setupButtons(round[0]);
     }
-
     // testing
 
-    private void setupButtons(int round) {
-        // Change the text of the next button to "Finish" if it's the last round
-        if (round == 4) {
-            buttonNext.setText(R.string.finish);
-            buttonNext.setOnClickListener(view -> {
-                Intent intent = new Intent(GameActivity.this, ScoreActivity.class);
-                intent.putExtra("correct", correctAnswer);
-                intent.putExtra("incorrect", incorrectAnswer);
-                startActivity(intent);
-            });
-        }
+    // Change status bar color
+    public void setHeaderColor() {
+        Window window = getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(Color.parseColor("#1984BF"));
+    }
 
-        // Set the image of the celebrity
-        imageViewCelebrity.setImageResource(resourceFiles[round]);
+    public void setFields() {
+        imageViewCelebrity = findViewById(R.id.imageView_Celebrity);
+        buttonAnswer1 = findViewById(R.id.button_Answer1);
+        buttonAnswer2 = findViewById(R.id.button_Answer2);
+        buttonAnswer3 = findViewById(R.id.button_Answer3);
+        buttonAnswer4 = findViewById(R.id.button_Answer4);
+        buttonNext = findViewById(R.id.button_Next);
+    }
 
-        // Shuffling the buttons
-        Collections.shuffle(buttons);
-        // Below allows colors to be preserved when rotating the device
-        if (!flagSavedInstance) {
-            // Loop through the buttons and set the text
-            for (int i = 0; i < buttons.size(); i++) {
-                buttons.get(i).setText(guesses[round][i]);
-            }
-        }
+    public void createButtonArray() {
+        // Initialize the buttons ArrayList
+        buttons = new ArrayList<>();
+        buttons.add(buttonAnswer1);
+        buttons.add(buttonAnswer2);
+        buttons.add(buttonAnswer3);
+        buttons.add(buttonAnswer4);
+    }
 
-        // Set the button color blue for each button
+    public void setButtonColor() {
         for (Button button : buttons) {
             Log.d("GameActivity", "Setting up button " + button.getText().toString());
-            if(button.getText().toString().equals("Correct!") )
+            if (button.getText().toString().equals("Correct!"))
                 button.setBackgroundColor(getResources().getColor(R.color.green, null));
-
-            else if(button.getText().toString().equals("Incorrect") )
+            else if (button.getText().toString().equals("Incorrect"))
                 button.setBackgroundColor(getResources().getColor(R.color.red, null));
-
             else
                 button.setBackgroundColor(getResources().getColor(R.color.blue, null));
         }
+    }
 
+    public void setOnClicks() {
         // Set up click listeners for each button
         for (Button button : buttons) {
             button.setOnClickListener(view -> {
-                if (button.getText().toString().equals(guesses[round][0])) {
+                if (button.getText().toString().equals(guesses[round[0]][0])) {
                     // This is the correct answer
                     button.setBackgroundColor(getResources().getColor(R.color.green, null));
                     button.setText(R.string.correct);
@@ -156,17 +138,58 @@ public class GameActivity extends AppCompatActivity {
                     button.setText(R.string.incorrect);
                     incorrectAnswer++;
                     Log.d("incorrectAnswer", "Incorrect answer: " + incorrectAnswer);
+                    // Turn off on click listener for the incorrect button
+                    button.setOnClickListener(null);
                 }
             });
         }
-        flagSavedInstance = false;
-
     }
+
+    public void setImageViewCelebrity() {
+        // Set the image of the celebrity
+        imageViewCelebrity.setImageResource(resourceFiles[round[0]]);
+    }
+
+    private void setupButtonsTxtNotOnRotate() {
+        // Shuffling the buttons
+        Collections.shuffle(buttons);
+        // Loop through the buttons and set the text
+        for (int i = 0; i < buttons.size(); i++) {
+            buttons.get(i).setText(guesses[round[0]][i]);
+        }
+    }
+
+    public void setNextButton() {
+        // Set up the next button
+        buttonNext.setOnClickListener(view -> {
+            // Increment the round
+            round[0]++;
+            Log.d("GameActivity", "Setting up buttons for round " + round[0]);
+            // Set up the buttons for the next round
+            setupButtonsTxtNotOnRotate();
+            setNextButtonIfLastRound();
+            setImageViewCelebrity();
+            setButtonColor();
+        });
+    }
+
+    public void setNextButtonIfLastRound() {
+        // Change the text of the next button to "Finish" if it's the last round
+        if (round[0] == 4) {
+            buttonNext.setText(R.string.finish);
+            buttonNext.setOnClickListener(view -> {
+                Intent intent = new Intent(GameActivity.this, ScoreActivity.class);
+                intent.putExtra("correct", correctAnswer);
+                intent.putExtra("incorrect", incorrectAnswer);
+                startActivity(intent);
+            });
+        }
+    }
+
     public static void resetGame() {
         round[0] = 0;
         correctAnswer = 0;
         incorrectAnswer = 0;
-        flagSavedInstance = false;
     }
 
 }
